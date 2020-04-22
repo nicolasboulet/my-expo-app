5e84728e1311db25404ee743c6177ec7bb4da6dc
Nicolas Boulet <nicolasboulet@live.fr>
2020-04-22 17:24:48 +0200

    add dependency

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test de mon app !!!</Text>
-    </View>
+      <Text>Test de mon app </Text>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
